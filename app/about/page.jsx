c221da509cba2381f8cfefb7feda9edba9ece05d
tangguangyuan <tangguangyuan@jcxx.com>
2024-04-24 16:52:03 +0800

    feat: add more pages using Page Route

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Version: 1.0
+ * @Author: tangguangyuan tangguangyuan@jcxx.com
+ * @Date: 2024-04-24 16:48:33
+ */
+export default function About() {
+    return (
+        <>
+            <h1>About page.</h1>
+            <p>This is about page. For more details</p>
+        </>
+    )
+}
---
+++ app/reviews/hollow-knight/page.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Version: 1.0
+ * @Author: tangguangyuan tangguangyuan@jcxx.com
+ * @Date: 2024-04-24 16:50:40
+ */
+export default function HolloKnightPage() {
+    return (
+        <>
+            <h1>Hollo Knight</h1>
+            <p>Hallo Knight is a game about a knight who is lost in the forest.</p>
+        </>
+    )
+}
