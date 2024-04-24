0daf8e4590e628738ca6b32bcc045ba17efab894
tangguangyuan <tangguangyuan@jcxx.com>
2024-04-24 16:47:21 +0800

    feat: learn about page route

---
+++ app/reviews/page.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Version: 1.0
+ * @Author: tangguangyuan tangguangyuan@jcxx.com
+ * @Date: 2024-04-24 16:18:40
+ */
+export default function ReviewPage() {
+    return (
+        <>
+            <h1>Reviews</h1>
+            <p>Here we'll list all the reviews.</p>
+        </>
+    )
+}
---
+++ app/reviews/stardew-valley/page.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Version: 1.0
+ * @Author: tangguangyuan tangguangyuan@jcxx.com
+ * @Date: 2024-04-24 16:14:32
+ */
+export default function StardewValley() {
+    return (
+        <div>
+            <h1>Stardew Valley</h1>
+            <p>This will b e the review for Stardew Valley.</p>
+        </div>
+    )
+}
