3b514edcbe550bd082a6dd85a89fc7371cbe5449
tangguangyuan <tangguangyuan@jcxx.com>
2024-04-24 15:29:46 +0800

    feat: add Layout comp

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @Version: 1.0
+ * @Author: tangguangyuan tangguangyuan@jcxx.com
+ * @Date: 2024-04-24 15:25:50
+ */
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
